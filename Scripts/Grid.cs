0ebdd70c27dbc8ae8c98cae8dae935132b56df73
Snory <robtill.in@gmail.com>
2020-06-03 16:07:48 +0200

    init

---
+++ Scripts/Grid.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+
+    private Node[,] _grid;
+    [SerializeField]
+    private Vector2 _gridWorldSize = new Vector2(200, 200);
+    [SerializeField]
+    private float _size = 1.0f;
+    private int  _gridSizeX, _gridSizeY;
+    private Camera _mainCamera;
+
+    [SerializeField]
+    private GameObject _test;
+
+
+    public float Size { get; set; }
+
+    private void Awake()
+    {
+
+        _gridSizeX = Mathf.RoundToInt(_gridWorldSize.x / _size);
+        _gridSizeY = Mathf.RoundToInt(_gridWorldSize.y / _size);
+        _grid = new Node[_gridSizeX, _gridSizeY];
+        _mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+       
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //nastavit startovní pozici vlevo nahoře
+        Vector3 worldBottomRight = this.transform.position + (Vector3.left * _gridWorldSize.x / 2) + (Vector3.down * _gridWorldSize.y / 2);
+
+        for(int i = 0; i < _gridSizeX; i++)
+        {
+            for(int j = 0; j < _gridSizeY; j++)
+            {
+                if(j%2 == 0) { 
+                    Instantiate(_test, worldBottomRight + new Vector3(i * _size, j * _size,0)  , Quaternion.identity);
+                } else
+                {
+                    GameObject hex = Instantiate(_test, worldBottomRight + new Vector3(i * (_size) + _size / 2, j * (_size), 0), Quaternion.identity);
+                    hex.GetComponent<SpriteRenderer>().color = Color.blue;
+                }
+            }
+        }
+
+
+
+
+       //vytvořit grid
+       //vykreslit grid
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Scripts/Node.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+
+    private Vector2 _worldSpaceCoordination;
+    private Vector2 _gridCoordination;
+
+    public Vector2 WorldSpaceCoordination { get; set; }
+    public Vector2 GridSpaceCoordination { get; set; }
+
+
+    public Node(Vector2 worldSpaceCoordination, Vector2 gridCoordination)
+    {
+        WorldSpaceCoordination = worldSpaceCoordination;
+    }
+
+    
+  
+}
