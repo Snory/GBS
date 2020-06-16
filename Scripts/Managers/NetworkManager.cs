52e1834af315ff472c9523e24844fa098c7077e1
Snory <robtill.in@gmail.com>
2020-06-16 18:32:06 +0200

    adding photon, connection to master server

---
+++ Scripts/Managers/NetworkManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+
+    private static NetworkManager _instance;
+    public static NetworkManager Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        } else
+        {            
+            Destroy(this.gameObject);            
+
+        }
+
+    }
+    private void Start()
+    {
+        //connect to the master server
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to server");
+
+    }
+}
